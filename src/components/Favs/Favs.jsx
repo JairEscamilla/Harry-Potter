740fe3b091698a845cd1431badc56b18fb06ef97
Jair Escamilla <hernandez_yo14@outlok.es>
2022-10-09 02:46:21 -0500

    Added favs panel

---
+++ src/components/Favs/Favs.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import './Favs.sass'
+
+export const Favs = ({ isOpened }) => {
+  return (
+    <div className={`favs ${isOpened ? 'opened' : 'closed'} `}>
+      <div className="fav">
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/149/149071.png"
+          alt="fav picture"
+          className="fav__picture"
+        />
+        <p className="fav__name">Luna Lovegood</p>
+        <span className="material-symbols-outlined">delete</span>
+      </div>
+    </div>
+  )
+}
--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,17 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Favs } from '../Favs/Favs'
 import './Menu.sass'
 
 export const Menu = ({ setIsModalOpen }) => {
+  const [favsAreOpened, setFavsAreOpened] = useState(false)
+
   return (
-    <ul className="menu">
-      <li>
-        <span>Favoritos</span>
-        <span class="material-symbols-outlined">bookmark</span>
-      </li>
-      <li onClick={() => setIsModalOpen(true)}>
-        <span>Agregar</span>
-        <span class="material-symbols-outlined">person_add</span>
-      </li>
-    </ul>
+    <div className="menu__container">
+      <ul className="menu">
+        <li onClick={() => setFavsAreOpened(!favsAreOpened)}>
+          <span>Favoritos</span>
+          <span className="material-symbols-outlined filled">bookmark</span>
+        </li>
+        <li onClick={() => setIsModalOpen(true)}>
+          <span>Agregar</span>
+          <span className="material-symbols-outlined filled">person_add</span>
+        </li>
+      </ul>
+
+      <Favs isOpened={favsAreOpened} />
+    </div>
   )
 }
